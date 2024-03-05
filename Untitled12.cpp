3c9dce3a9236f9328cdd07b4a0c363e1a8b8e372
chandupoola181620 <124548552+chandupoola181620@users.noreply.github.com>
2024-03-05 14:24:22 +0530

    Add files via upload

---
+++ Untitled12.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+	int num;
+	printf("Enter an  number: ");
+	scanf("%d",&num);
+	
+	if(num % 2 == 0)
+	{
+		printf("Enter of an even number: ")
+	}
+	else
+	{
+		printf("Enter of an odd number: ")
+	}
+	
+	return 0;
+}
+
+	
